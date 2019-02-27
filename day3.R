c52c8917f24b8377e51cdbe9bde0e24220b32a80
Richard Pilbery <tricky999@Richards-MacBook-Pro.local>
2019-02-27 16:48:11 +0000

    Day 3 stuff

---
+++ day3.R
@@ -0,0 +1,5 @@
+# Iron Law of Evaluation - Rossi
+# Successful schemes hard to reproduce elsewhere
+
+# Decision needed over what ratio if matching with replacement (IAU use 1:1)
+# Should you use local or non-local controls
